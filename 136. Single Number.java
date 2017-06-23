752eab40fbd6e465f516f66314b06ac7109c0b76
Xiaochun Ma <122030478@qq.com>
2017-06-24 00:30:21 +0800

    136. Single Number
    Jump every two numbers, 7 ms, beat 24.39%.

---
+++ 136. Single Number.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length -2; i = i + 2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
